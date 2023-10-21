416166863cb64a222e4b3164b1843d27c7cd3a2c
Łukasz Michalak <lupinummail@gmail.com>
2023-10-21 22:07:38 +0200

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Łukasz Michalak on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
